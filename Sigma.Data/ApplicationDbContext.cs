5fd380c6733f237cac5913c9a617e92ab429a93d
Ravi <rblucky3@gmail.com>
2025-01-19 10:38:53 +0545

    add ApplicationDbContext to establish connection with database initialize Candidates DbSet entity

---
+++ Sigma.Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Sigma.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sigma.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Candidate> Candidates { get; set; }
+
+        // Overriding OnModelCreating to configure the entity model
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Ensuring that the Email field is unique
+            modelBuilder.Entity<Candidate>()
+                .HasIndex(c => c.Email)  // Creating an index on the Email field
+                .IsUnique();  // Making the index unique
+
+            base.OnModelCreating(modelBuilder);
+        }
+        
+
+    }
+}
