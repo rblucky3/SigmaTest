fea549f4a9962b0f597b07a7cddd181616b0d309
Ravi <rblucky3@gmail.com>
2025-01-19 10:54:03 +0545

    add repository layer to perform Insert and Update on Candidate entity

---
+++ Sigma.Repository/Settings/Interfaces/ICandidateRepository.cs
@@ -0,0 +1,13 @@
+﻿using Sigma.Core.Entities;
+
+namespace Sigma.Repository.Settings.Interfaces
+{
+    public interface ICandidateRepository
+    {
+        Task<List<Candidate>> GetAllAsync();
+        Task<Candidate> GetByEmailAsync(string email);
+        Task AddAsync(Candidate candidate);
+        Task UpdateAsync(Candidate candidate);
+        Task SaveChangesAsync();
+    }
+}
---
+++ Sigma.Repository/Settings/Repositories/CandidateRepository.cs
@@ -0,0 +1,43 @@
+﻿using Sigma.Data;
+using Sigma.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Sigma.Repository.Settings.Interfaces;
+
+namespace Sigma.Repository.Settings.Repositories
+{
+    public class CandidateRepository : ICandidateRepository
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public CandidateRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Candidate>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<Candidate> GetByEmailAsync(string email)
+        {
+            return await _context.Candidates.FirstOrDefaultAsync(c => c.Email == email);
+        }
+
+        public async Task AddAsync(Candidate candidate)
+        {
+            await _context.Candidates.AddAsync(candidate);
+        }
+
+        public async Task UpdateAsync(Candidate candidate)
+        {
+            _context.Candidates.Update(candidate);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+       
+    }
+}
