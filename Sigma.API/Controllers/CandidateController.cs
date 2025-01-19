f31d0981cb7c8fd784b565e454da394096da9eee
Ravi <rblucky3@gmail.com>
2025-01-19 11:23:45 +0545

    add API endpoint i.e. method in Candidate controller for insert and update operation

---
+++ Sigma.API/Controllers/CandidateController.cs
@@ -0,0 +1,52 @@
+﻿using Sigma.API.Model;
+using Sigma.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Sigma.Service.Settings.Interfaces;
+
+namespace Sigma.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        private readonly ICandidateService _candidateService;
+
+        public CandidateController(ICandidateService candidateService)
+        {
+            _candidateService = candidateService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddOrUpdateCandidate([FromBody] Candidate candidate)
+        {
+            if (!ModelState.IsValid || candidate == null)
+            {
+                // Collect validation error messages
+                var validationMessages = ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
+
+                // Return a BadRequest with validation errors
+                return BadRequest(new ApiResponse
+                {
+                    StatusCode = 400,
+                    Message = "Validation failed.",
+                    ValidationMessages = validationMessages
+                });
+            }
+
+            await _candidateService.AddOrUpdateCandidateAsync(candidate);
+
+            var successResponse = new ApiResponse
+            {
+                StatusCode = 200,
+                Message = "Candidate  added/updated successfully!",
+
+            };
+
+            return Ok(successResponse);
+        }
+    }
+}
