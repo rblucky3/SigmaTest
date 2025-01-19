29b3169d128fe8d946974a91c1abfc9710e1ba27
Ravi <rblucky3@gmail.com>
2025-01-19 11:39:32 +0545

    add migrations for creating Database and Candidates tables automatically

---
+++ Sigma.Data/Migrations/20250119055034_InitialDbCreation.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sigma.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDbCreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    CallTimeInterval = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LinkedInProfileUrl = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    GitHubProfileUrl = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candidates_Email",
+                table: "Candidates",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidates");
+        }
+    }
+}
