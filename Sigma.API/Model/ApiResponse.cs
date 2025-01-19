1682936739bc0e565585e5c6119289df4809805f
Ravi <rblucky3@gmail.com>
2025-01-19 11:13:18 +0545

    add model class for Custom API Responses

---
+++ Sigma.API/Model/ApiResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace Sigma.API.Model
+{
+    public class ApiResponse
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public List<string> ValidationMessages { get; set; }
+
+        public ApiResponse()
+        { }
+
+        public ApiResponse(int statusCode, string message, List<string> validationMessages = null)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            ValidationMessages = validationMessages ?? new List<string>();
+        }
+    }
+}
