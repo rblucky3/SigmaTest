1b24b9f2a3cac1e4fdecd1f6efa662b15f6e053b
Ravi <rblucky3@gmail.com>
2025-01-19 11:28:18 +0545

    register repositories and services

--- Sigma.API/Program.cs
+++ Sigma.API/Program.cs
@@ -1,8 +1,25 @@
+using Sigma.Data;
+using Microsoft.EntityFrameworkCore;
+using Sigma.Service.Settings.Services;
+using Sigma.Service.Settings.Interfaces;
+using Sigma.Repository.Settings.Interfaces;
+using Sigma.Repository.Settings.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configure DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+
+// Register repository and service
+builder.Services.AddScoped<ICandidateRepository, CandidateRepository>();
+builder.Services.AddScoped<ICandidateService, CandidateService>();
+
+
+// Add services to the container.
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,9 +33,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
